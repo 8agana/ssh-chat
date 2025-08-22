aebc8818d581655b355de57039b87f796c2c51b8
8agana <sam.atagana@gmail.com>
2025-08-22 17:51:22 -0500

    Initial Commit

---
+++ ssh-chat/ssh_chatApp.swift
@@ -0,0 +1,32 @@
+//
+//  ssh_chatApp.swift
+//  ssh-chat
+//
+//  Created by Samuel Atagana on 8/22/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ssh_chatApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ ssh-chatTests/ssh_chatTests.swift
@@ -0,0 +1,17 @@
+//
+//  ssh_chatTests.swift
+//  ssh-chatTests
+//
+//  Created by Samuel Atagana on 8/22/25.
+//
+
+import Testing
+@testable import ssh_chat
+
+struct ssh_chatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
